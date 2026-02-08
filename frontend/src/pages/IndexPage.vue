119d964e77e673faa7e98049ceb73456ae0998fa
Prawit408 <loveffza11@gmail.com>
2026-02-08 22:47:56 +0700

    chore: finalize docker compose with fullstack support

---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="text-h6">Welcome to Quasar App</div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
